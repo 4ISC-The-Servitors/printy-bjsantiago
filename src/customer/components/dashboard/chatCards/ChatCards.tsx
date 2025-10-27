9ad06c0b0320d7ece3be12f196cf9907d35a10f5
Andrea Louise Salazar <andrealouise.salazar.cics@ust.edu.ph>
2025-10-27 20:55:50 +0800

    commented out PLACE ORDER flow for now

--- src/customer/components/dashboard/chatCards/ChatCards.tsx
+++ src/customer/components/dashboard/chatCards/ChatCards.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ServicesOffered from './ServicesOffered';
-import PlaceAnOrder from './PlaceAnOrder';
+// import PlaceAnOrder from './PlaceAnOrder';
 import AskAssistance from './AskAssistance';
 import AskQuote from './AskQuote';
 import AboutUs from './AboutUs';
@@ -15,7 +15,7 @@ const ChatCards: React.FC<ChatCardsProps> = ({ onSelect }) => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-5 md:gap-4 lg:gap-5">
       <ServicesOffered onClick={() => onSelect('servicesOffered')} />
       <AskQuote onClick={() => onSelect('askQuote')} />
-      <PlaceAnOrder onClick={() => onSelect('placeOrder')} />
+      {/* <PlaceAnOrder onClick={() => onSelect('placeOrder')} /> */}
       <AskAssistance onClick={() => onSelect('issueTicket')} />
       <AboutUs onClick={() => onSelect('aboutUs')} />
       <FAQs onClick={() => onSelect('faqs')} />
